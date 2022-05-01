5e306aef7eeecfbcd587aaea7c44d1506855ad0d
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-01 12:31:23 -0300

    Add Movie genre association and update_rating method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,14 @@
 class Movie < ApplicationRecord
+  has_and_belongs_to_many :genres
   has_many :ratings
+
   validates :title, :release_date, :runtime, presence: true
   validates :runtime, numericality: { only_integer: true,
                                       greater_than: 0 }
 
   enum parental_rating: %i[g pg pg_13 r nc_17]
+
+  def update_rating
+    update_column(:rating, ratings.average(:grade))
+  end
 end
