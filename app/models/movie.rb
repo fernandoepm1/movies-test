79297a750606248ee616e95adeadbae870579c51
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-01 12:28:41 -0300

    Update Movie model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   has_many :ratings
+  validates :title, :release_date, :runtime, presence: true
+  validates :runtime, numericality: { only_integer: true,
+                                      greater_than: 0 }
+
 end
