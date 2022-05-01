abefa5a673c1dfaa4c7b8fb7faf1c294abec8d8f
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-01 12:46:28 -0300

    Update ratings create endpoint params

--- app/api/ratings/api.rb
+++ app/api/ratings/api.rb
@@ -1,21 +1,25 @@
+# frozen_string_literal: true
+
 module Ratings
   class API < Grape::API
     version 'v1', using: :path
-    prefix "api"
+    prefix 'api'
     format :json
 
     resource :ratings do
       desc 'add ratings to a movie'
       params do
-        requires :movie_id, type: String, desc: 'Movie id.'
+        requires :movie_id, type: String, desc: 'Movie ID.'
         requires :grade, type: Integer, desc: 'Movie grade.'
       end
 
       post do
-        Rating.create!({
-          movie_id: params[:movie_id],
-          grade: params[:grade]
-        })
+        Rating.create!(
+          {
+            movie_id: params[:movie_id],
+            grade: params[:grade]
+          }
+        )
       end
     end
   end
