80055492d70e660ef71c0f6457e41c004a052d94
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-01 12:34:10 -0300

    Update Movie unit tests and add new ones

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should have_and_belong_to_many(:genres) }
+    it { should have_many(:ratings) }
+  end
 
-  it { is_expected.to have_many :ratings }
+  context 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:release_date) }
+    it { should validate_presence_of(:runtime) }
+    it { should validate_numericality_of(:runtime).only_integer
+                                                  .is_greater_than(0) }
+  end
+
+  it { should define_enum_for(:parental_rating).with(%i[g pg pg_13 r nc_17]) }
 end
