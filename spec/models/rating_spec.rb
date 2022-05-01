ac407779b23fc32b0f1ad2e47cabf4e99480e3e4
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-01 12:33:56 -0300

    Update Rating unit tests and add new ones

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,15 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  it { is_expected.to belong_to :movie }
-
-  it 'grade cannnot be less than 0' do
-    rating = FactoryBot.build(:rating, grade: -1)
-    expect(rating.valid?).to eq false
+  context 'associations' do
+    it { should belong_to :movie }
   end
 
-  it 'grade cannnot be more than 5' do
-    rating = FactoryBot.build(:rating, grade: 6)
-    expect(rating.valid?).to eq false
+  context 'validations' do
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:grade) }
+    it do
+      should validate_numericality_of(:grade).only_integer
+                                             .is_greater_than(0)
+                                             .is_less_than(6)
+    end
+  end
+
+  describe '#update_movie_rating' do
+    subject(:movie) { create(:movie) }
+
+    context 'when rating is created' do
+      it 'should update movie rating' do
+        create(:rating, movie: movie, grade: 5)
+
+        expect(movie.reload.rating).to equal(5.0)
+      end
+    end
+
+    context 'when rating is updated' do
+      let(:rating) { create(:rating, movie: movie, grade: 5) }
+
+      it 'should update movie rating' do
+        rating.grade = 3
+        rating.save
+
+        expect(movie.reload.rating).to equal(3.0)
+      end
+    end
   end
 end
