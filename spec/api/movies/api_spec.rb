e34d0e16f752980da4cf5a2b1da3ff560d4c1a9a
Fernando Eufrásio <fernando.epm1@gmail.com>
2022-05-02 18:30:02 -0300

    Update all api specs and create new ones

--- spec/api/movies/api_spec.rb
+++ spec/api/movies/api_spec.rb
@@ -1,47 +1,219 @@
 require 'rails_helper'
 
 RSpec.describe Movies::API do
-  let!(:movie) { Movie.create(title: "teste", release_date: Date.today) }
-  let!(:movie2) { Movie.create(title: "teste2", release_date: Date.tomorrow) }
-
   describe 'GET /api/v1/movies' do
-    it 'returns all movies' do
-      get "/api/v1/movies/"
+    let!(:movies) { create_list(:movie, 7) }
+
+    before(:each) do
+      get '/api/v1/movies'
+    end
+
+    it 'should have http status ok' do
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("teste")
-      expect(response.body).to include("teste2")
+    end
+
+    it 'should paginate the response with 5 per page' do
+      expect(JSON.parse(response.body).size).to eq(5)
+    end
+
+    it 'should return the first 5 movies' do
+      movies.first(5).each do |movie|
+        expect(response.body).to include(movie.title)
+      end
     end
   end
 
   describe 'GET /api/v1/movies/search' do
-    it 'returns searched movie' do
-      get "/api/v1/movies/search?title=2"
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include("teste2")
+    let!(:movie)  { create(:movie, title: 'Titanic 2') }
+    let!(:movie2) { create(:movie, title: 'Avatar 2') }
+    let!(:movie3) { create(:movie, title: 'Ex Machina') }
+
+    before(:each) do
+      get "/api/v1/movies/search?#{query_params}"
+    end
+
+    context 'when no movies match the search' do
+      let(:query_params) { 'title=Thunder' }
+
+      it 'should have http status 500' do
+        expect(response).to have_http_status(:internal_server_error)
+      end
+
+      it 'should return an error message' do
+        expect(response.body).to include('error')
+        expect(response.body).to include('No movies found')
+      end
+    end
+
+    context 'when movies match the search' do
+      let(:query_params) { 'title=2' }
+
+      it 'should have http status 200' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should return both movies matching the search' do
+        expect(response.body).to include('Titanic 2')
+        expect(response.body).to include('Avatar 2')
+      end
     end
   end
 
   describe 'GET /api/v1/movies/:id' do
-    it 'returns movie using id' do
-      get "/api/v1/movies/#{movie2.id}"
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include("teste2")
+    before(:each) do
+      get "/api/v1/movies/#{movie_id}"
+    end
+
+    context 'when no movie is found' do
+      let(:movie_id) { 0 }
+
+      it 'should have http status 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'should return an error message' do
+        expect(response.body).to include('error')
+        expect(response.body).to include("Couldn't find Movie")
+      end
+    end
+
+    context 'when the movie exists' do
+      let!(:movie) { create(:movie, title: 'Fast and Furious 12') }
+      let(:movie_id) { movie.id }
+
+      it 'should have http status 200' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should return the movie with the given ID' do
+        expect(response.body).to include(movie.title)
+      end
     end
   end
 
   describe 'POST /api/v1/movies' do
-    it 'returns movie using id' do
-      post "/api/v1/movies", :params => { :title => "Any Name", release_date: Date.yesterday }
-      expect(response).to have_http_status(:created)
-      expect(Movie.count).to eq(3)
+    before(:each) do
+      post '/api/v1/movies', params: params
+    end
+
+    context 'with invalid movie params' do
+      context 'with a missing title' do
+        let(:params) do
+          { release_date: Date.today, runtime: 123 }
+        end
+
+        it 'should have http status 406' do
+          expect(response).to have_http_status(:not_acceptable)
+        end
+
+        it 'should not create a new movie in the database' do
+          expect(Movie.count).to eq(0)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('title is missing')
+        end
+      end
+
+      context 'with a missing release date' do
+        let(:params) do
+          { title: 'Dune', runtime: 123 }
+        end
+
+        it 'should have http status 406' do
+          expect(response).to have_http_status(:not_acceptable)
+        end
+
+        it 'should not create a new movie in the database' do
+          expect(Movie.count).to eq(0)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('release_date is missing')
+        end
+      end
+
+      context 'with a missing runtime' do
+        let(:params) do
+          { title: 'Tokyo Drift', release_date: '2006-08-11' }
+        end
+
+        it 'should have http status 406' do
+          expect(response).to have_http_status(:not_acceptable)
+        end
+
+        it 'should not create a new movie in the database' do
+          expect(Movie.count).to eq(0)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('runtime is missing')
+        end
+      end
+    end
+
+    context 'with valid movie params' do
+      let(:params) do
+        {
+          title: 'Kill Bill Vol. 3',
+          release_date: Date.today.next_year(13),
+          runtime: 123
+        }
+      end
+
+      it 'should have http status 201' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should create a new movie in the database' do
+        expect(Movie.count).to eq(1)
+        expect(Movie.last.title).to eq(params[:title])
+      end
+
+      it 'should return a movie with the given params' do
+        expect(response.body).to include(params[:title])
+        expect(response.body).to include(params[:release_date].to_s)
+        expect(response.body).to include(params[:runtime].to_s)
+      end
     end
   end
 
   describe 'DELETE /api/v1/movies' do
-    it 'remove movie' do
-      delete "/api/v1/movies/#{movie2.id}"
-      expect(response).to have_http_status(:ok)
-      expect(Movie.count).to eq(1)
+    before(:each) do
+      delete "/api/v1/movies/#{movie_id}"
+    end
+
+    context 'when no movie is found' do
+      let(:movie_id) { 0 }
+
+      it 'should have http status 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'should return an error message' do
+        expect(response.body).to include('error')
+        expect(response.body).to include("Couldn't find Movie")
+      end
+    end
+
+    context 'when the movie exists' do
+      let!(:movie) { create(:movie, title: 'Tenacious D') }
+      let(:movie_id) { movie.id }
+
+      it 'should have http status 200' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should remove the movie from the database' do
+        expect(Movie.count).to eq(0)
+      end
+
+      it 'should return the deleted movie' do
+        expect(response.body).to include(movie.title)
+      end
     end
   end
 end
--- spec/api/ratings/api_spec.rb
+++ spec/api/ratings/api_spec.rb
@@ -1,13 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe Ratings::API do
-  let!(:movie) { Movie.create(title: "teste", release_date: Date.today) }
+  let!(:movie) { create(:movie, :with_genre) }
 
   describe 'POST /api/v1/ratings' do
-    it 'adds rating to movie' do
-      post "/api/v1/ratings", :params => { :movie_id => movie.id, grade: 2 }
-      expect(response).to have_http_status(:created)
-      expect(movie.ratings.count).to eq(1)
+    before(:each) do
+      post '/api/v1/ratings', params: params
+    end
+
+    context 'with invalid rating params' do
+      context 'when no movie_id is provided' do
+        let(:params) do
+          { grade: 2 }
+        end
+
+        it 'should have http status not acceptable' do
+          expect(response).to have_http_status(:not_acceptable)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('movie_id is missing')
+        end
+
+        it 'should not create a new rating' do
+          expect(movie.ratings.count).to eq(0)
+        end
+      end
+
+      context 'when no grade is provided' do
+        let(:params) do
+          { movie_id: movie.id }
+        end
+
+        it 'should have http status not acceptable' do
+          expect(response).to have_http_status(:not_acceptable)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('grade is missing')
+        end
+
+        it 'should not create a new rating' do
+          expect(movie.ratings.count).to eq(0)
+        end
+      end
+
+      context 'when an invalid movie_id is provided' do
+        let(:params) do
+          { movie_id: 0, grade: 5 }
+        end
+
+        it 'should have http status not acceptable' do
+          expect(response).to have_http_status(:internal_server_error)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('Movie must exist')
+        end
+
+        it 'should not create a new rating' do
+          expect(movie.ratings.count).to eq(0)
+        end
+      end
+
+      context 'when an invalid grade is provided' do
+        let(:params) do
+          { movie_id: movie.id, grade: -1 }
+        end
+
+        it 'should have http status not acceptable' do
+          expect(response).to have_http_status(:internal_server_error)
+        end
+
+        it 'should return an error message' do
+          expect(response.body).to include('error')
+          expect(response.body).to include('Grade must be greater than 0')
+        end
+
+        it 'should not create a new rating' do
+          expect(movie.ratings.count).to eq(0)
+        end
+      end
+    end
+
+    context 'with valid rating params' do
+      let(:params) do
+        { movie_id: movie.id, grade: 5 }
+      end
+
+      it 'should have http status created' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should not return an error message' do
+        expect(response.body).to_not include('error')
+      end
+
+      it 'should create a new rating for the movie' do
+        expect(movie.ratings.count).to eq(1)
+      end
     end
   end
 end
